685b0b305a20d98f3540073833a892c994abc76e
femstld <femstld@outlook.com>
2021-01-14 20:40:08 +0100

    start from linux

--- test.cpp
+++ test.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-int main()
-{
-   vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-   for (const string& word : msg)
-   {
-      cout << word << " ";
-   }
-   cout << endl;
+int main() {
+    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    char p='a';
+    getchar();
+    return 0;
 }
